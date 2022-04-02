882dd92e8d1115bdff316c5b4b9b73cefa12bb4a
Zaynox34 <85759934+Zaynox34@users.noreply.github.com>
2022-04-02 20:07:05 +0200

    son

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -31,17 +31,41 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.forward = transform.position.normalized;
         Move();
+        Patapon();
         //Targeter();
     }
     public void Move()
     {
+        transform.forward = transform.position.normalized;
         Vector2 move = playerControl.BaseControl.Move.ReadValue<Vector2>();
         if (move != Vector2.zero)
         {
             centerPivotTransform.Rotate(new Vector3(0, -move.x, 0) * Time.deltaTime * speed);
             transform.position += transform.forward*-move.y * Time.deltaTime * speed / Mathf.PI;
+        }
+    }
+    public void Patapon()
+    {
+        if(playerControl.BaseControl.North.triggered)
+        {
+            GetComponents<AudioSource>()[0].Play();
+        }
+        if (playerControl.BaseControl.East.triggered)
+        {
+            GetComponents<AudioSource>()[1].Play();
+        }
+        if (playerControl.BaseControl.West.triggered)
+        {
+            GetComponents<AudioSource>()[2].Play();
+        }
+        if (playerControl.BaseControl.South.triggered)
+        {
+            GetComponents<AudioSource>()[3].Play();
+        }
+        if (playerControl.BaseControl.Gatotsu.triggered)
+        {
+            GetComponents<AudioSource>()[4].Play();
         }
     }
     /*public void Targeter()
